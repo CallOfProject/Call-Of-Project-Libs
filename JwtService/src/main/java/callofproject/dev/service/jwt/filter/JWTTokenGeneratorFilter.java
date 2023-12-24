1f814c0bd7f00c5b65f1e611702ca046cccd4a67
nuricanozturk01 <18070006034@stu.yasar.edu.tr>
2023-12-24 18:46:30 +0300

    added error message to filters

--- JwtService/src/main/java/callofproject/dev/service/jwt/filter/JWTTokenGeneratorFilter.java
+++ JwtService/src/main/java/callofproject/dev/service/jwt/filter/JWTTokenGeneratorFilter.java
@@ -5,33 +5,44 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.HashMap;
 
 public class JWTTokenGeneratorFilter extends OncePerRequestFilter
 {
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response, FilterChain filterChain)
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException
     {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (null != authentication)
+        try
         {
-            var claims = JwtUtil.populateAuthorities(authentication.getAuthorities());
-
-            var map = new HashMap<String, Object>();
-            map.put("authorities", claims);
-            var jwt = JwtUtil.generateToken(map, authentication.getName());
-            response.setHeader("Authorization", jwt);
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if (null != authentication)
+            {
+                String claims = JwtUtil.populateAuthorities(authentication.getAuthorities());
+                HashMap<String, Object> map = new HashMap<>();
+                map.put("authorities", claims);
+                String jwt = JwtUtil.generateToken(map, authentication.getName());
+                response.setHeader("Authorization", jwt);
+                filterChain.doFilter(request, response);
+            }
+            else
+            {
+                throw new AccessDeniedException("Token not found or user not authenticated!");
+            }
+        } catch (AccessDeniedException ex)
+        {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.getWriter().write("{\"error\": \"" + ex.getMessage() + "\"}");
         }
-
-        filterChain.doFilter(request, response);
     }
 
     @Override
--- JwtService/src/main/java/callofproject/dev/service/jwt/filter/JWTTokenValidatorFilter.java
+++ JwtService/src/main/java/callofproject/dev/service/jwt/filter/JWTTokenValidatorFilter.java
@@ -6,6 +6,8 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.AuthorityUtils;
@@ -21,26 +23,24 @@ public class JWTTokenValidatorFilter extends OncePerRequestFilter
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException
     {
-        var jwt = request.getHeader("Authorization");
-        if (null != jwt)
-        {
-            try
-            {
+        try {
+            String jwt = request.getHeader("Authorization");
+            if (jwt != null && jwt.startsWith("Bearer ")) {
                 jwt = jwt.substring(7);
-                var username = JwtUtil.extractUsername(jwt);
-
-                var authorities = (String) JwtUtil.extractRoles(jwt);
-
-                var auth = new UsernamePasswordAuthenticationToken(username, null, AuthorityUtils.
-                        commaSeparatedStringToAuthorityList(authorities));
+                String username = JwtUtil.extractUsername(jwt);
+                String authorities = JwtUtil.extractRoles(jwt);
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
+                        username, null, AuthorityUtils.commaSeparatedStringToAuthorityList(authorities));
                 SecurityContextHolder.getContext().setAuthentication(auth);
-            } catch (Exception e)
-            {
-                throw new BadCredentialsException("Invalid Token received!");
+            } else {
+                throw new AccessDeniedException("Token not found or user not authenticated!");
             }
-
+            filterChain.doFilter(request, response);
+        } catch (BadCredentialsException | AccessDeniedException e) {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.getWriter().write("{\"error\": \"" + e.getMessage() + "\"}");
         }
-        filterChain.doFilter(request, response);
     }
 
     @Override
